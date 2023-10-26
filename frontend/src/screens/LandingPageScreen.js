8f7ad2f4005c1a573923a2a455b1535b2c330c4c
Ata Tuzuner <atatuzuner61@gmail.com>
2023-10-25 21:00:17 -0400

    Event card component created.

--- frontend/src/screens/LandingPageScreen.js
+++ frontend/src/screens/LandingPageScreen.js
@@ -3,15 +3,16 @@ import { Text, StyleSheet, View, TextInput, Button } from "react-native";
 import HeaderBar from "../components/HeaderBar";
 import { useSelector, useDispatch } from "react-redux";
 import { setUserData } from "../store/Action";
+import EventCard from "../components/EventCard";
 
 const LandingPageScreen = function () {
-  const [text, setText] = useState("");
+  // const [text, setText] = useState("");
 
   return (
-    <View>
+    <View style={{ backgroundColor: "white", flex: 1 }}>
       <HeaderBar title="Landing Page" align="center" />
-      <Text style={{ padding: 20 }}>Hellooo Welcome</Text>
-      <Text style={{ padding: 20, paddingTop: 0 }}> What's your name?</Text>
+      <EventCard />
+      {/* <Text style={{ padding: 20, paddingTop: 0 }}> What's your name?</Text>
       <TextInput
         label="Email"
         placeholder="Your input here..."
@@ -28,8 +29,8 @@ const LandingPageScreen = function () {
         onChangeText={(text) => {
           setText(text);
         }}
-      />
-      <Text style={{ padding: 20 }}>User Input: {text}</Text>
+      /> */}
+      {/* <Text style={{ padding: 20 }}>User Input: {text}</Text> */}
     </View>
   );
 };
