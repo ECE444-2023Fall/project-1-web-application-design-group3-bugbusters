a813c8b30f7398a12d122240df46f8fff64d0e6e
goelbenj <ben_goel@rogers.com>
2023-10-25 20:23:12 -0400

    Fix git parsing

--- flask-server/src/flask_server/global_config.py
+++ flask-server/src/flask_server/global_config.py
@@ -12,8 +12,5 @@ search_client = AlgoliaSearchClient('GS4ISHV4RC', os.getenv('ALGOLIA_API_KEY'), 
 
 # Instantiate the DB
 # Authenticate credentials so we can access our firebase project
-git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
-repo_root = git_repo.git.rev_parse("--show-toplevel")
-json_path = "flask-server/ece444bulletin-firebase-adminsdk.json"
-json_path = os.path.join(repo_root, json_path)
+json_path = "ece444bulletin-firebase-adminsdk.json"
 db_client = DataBaseClient(json_path)
