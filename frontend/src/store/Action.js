f645bf8a60c73cf8d7bc0def85dcbed140fb41da
Ata Tuzuner <atatuzuner61@gmail.com>
2023-10-31 18:11:25 -0400

    Event details design partially finished. Need to implement image swiping.

--- frontend/src/store/Action.js
+++ frontend/src/store/Action.js
@@ -19,6 +19,13 @@ export const setCurrentEventData = (value) => ({
   payload: value,
 });
 
+// CurrentEventData
+import { SETCURRENTEVENTDATA } from "./ActionType";
+export const setCurrentEventData = (value) => ({
+  type: SETCURRENTEVENTDATA,
+  payload: value,
+});
+
 // Reset
 import { RESET } from "./ActionType";
 export const reset = (value) => ({
