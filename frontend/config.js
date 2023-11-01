406db353c8c6ac9879c49f796292c5d84d25c585
Ata Tuzuner <atatuzuner61@gmail.com>
2023-11-01 19:18:09 -0400

    Added config for firebase authentication

---
+++ frontend/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnK_EgAoRtEyaSWbq_uhvc-rCVQjBzqz4",
+  authDomain: "ece444bulletin.firebaseapp.com",
+  projectId: "ece444bulletin",
+  storageBucket: "ece444bulletin.appspot.com",
+  messagingSenderId: "1000140561380",
+  appId: "1:1000140561380:web:a35263774a0ded3ca345f1",
+  measurementId: "G-3V4BS6SWNN",
+};
+
+export default firebaseConfig;
