c63bf6c8293ddc8f0cdc6255391905ccedf06ae5
Ata Tuzuner <atatuzuner61@gmail.com>
2023-11-02 11:57:27 -0400

    Comment removed

--- frontend/src/navigation/BottomTabNavigator.js
+++ frontend/src/navigation/BottomTabNavigator.js
@@ -55,7 +55,6 @@ export default function BottomTab({ navigation }) {
         }}
         listeners={{
           tabPress: (e) => {
-            // Prevent default action
             e.preventDefault();
             navigation.navigate("Create/Edit Event");
           },
