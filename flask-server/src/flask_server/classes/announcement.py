db98c73dd10697d7236e35e370bab6f1453d87e2
goelbenj <ben_goel@rogers.com>
2023-11-06 20:38:34 -0500

    Add Announcement class

---
+++ flask-server/src/flask_server/classes/announcement.py
@@ -0,0 +1,37 @@
+from flask_server.classes.common import ClassField, DataField
+
+
+ANNOUNCEMENT_FIELDS = DataField([
+    ClassField("description"),
+    ClassField("timestamp"),
+])
+
+
+class Announcement:
+    """Announcement class"""
+    def __init__(self, description, timestamp=None):
+        self.description = description
+        self.timestamp = timestamp
+
+    @classmethod
+    def from_json(cls, json):
+        # check that all required fields exist and no extra kwargs exist
+        announcement_instance = cls(**json)
+
+        for key, value in json.items():
+            factory_func = ANNOUNCEMENT_FIELDS.factory_funcs(key)
+            value = factory_func(value)
+            setattr(announcement_instance, key, value)
+
+        return announcement_instance
+
+    def to_json(self):
+        announcement_json = {}
+
+        for field_name in ANNOUNCEMENT_FIELDS:
+            announcement_value = eval(f"self.{field_name}")
+            if announcement_value is not None:
+                json_factory_func = ANNOUNCEMENT_FIELDS.json_factory_funcs(field_name)
+                announcement_json[field_name] = json_factory_func(announcement_value)
+
+        return announcement_json
