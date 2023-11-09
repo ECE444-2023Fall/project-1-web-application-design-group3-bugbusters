1c47db34a380967dd402f0b2b9401e205ff71102
Chris Mountain <ccallammountain@gmail.com>
2023-11-09 12:56:38 -0500

    add search start_time and end_time to search

--- flask-server/src/flask_server/classes/search_client.py
+++ flask-server/src/flask_server/classes/search_client.py
@@ -1,4 +1,5 @@
 from algoliasearch.search_client import SearchClient
+from datetime import datetime
 
 class AlgoliaSearchClient:
     
@@ -20,42 +21,88 @@ class AlgoliaSearchClient:
         content["_tags"] = data.get("_tags", [])
         content["header_image_URL"] = data.get('_header_image', '')
         content["friendly_creator_name"] = data.get('_friendly_creator_name', '')
+        content["reported"] = False
 
         self.index.save_object(content)
 
-    def search_index(self, query, filters=""):
+    def search_index(self, query, filters="", start_time=None, end_time=None):
         """
         Search the index.
         """
+        if start_time == None:
+            start_time = float('-inf')
+        if end_time == None:
+            end_time = float('inf')
+
         if(filters == ""):
             search_result = self.index.search(query)
         else:
             search_result = self.index.search(query, {"filters": filters})
 
         return_result = {}
-        return_result['nbHits'] = search_result['nbHits']
         return_result['results'] = []
         for result in search_result['hits']:
-            obj = {}
-            obj['description'] = result.get('description', '')
-            obj['event_title'] = result.get('event_title', '')
-            obj['header_image_URL'] = result.get('header_image_URL', '')
-            obj['friendly_creator_name'] = result.get('friendly_creator_name', '')
-            return_result['results'].append(obj)
+            event_start_time = datetime.strptime(result['event_start_time'], "%Y-%m-%dT%H:%M:%S.%f").timestamp()
+            event_end_time = datetime.strptime(result['event_end_time'], "%Y-%m-%dT%H:%M:%S.%f").timestamp()
+            if start_time > event_start_time or end_time < event_end_time:
+                continue
+
+            else:
+                obj = {}
+                obj['description'] = result.get('description', '')
+                obj['event_title'] = result.get('event_title', '')
+                obj['header_image_URL'] = result.get('header_image_URL', '')
+                obj['friendly_creator_name'] = result.get('friendly_creator_name', '')
+                obj['start_time'] = result.get('event_start_time', '')
+                obj['end_time'] = result.get('event_end_time', '')
+                return_result['results'].append(obj)
         
+        return_result['nbHits'] = len(return_result['results'])
         return return_result
 
-    def update_index(self, event_id, content):
+    def update_index(self, event_id, data):
         """
         Partially update an object in the index.
         """
+        content = {}
         content["objectID"] = event_id
-        content["event_title"] = content.get("_event_title", "")
-        content["description"] = content.get("_description", "")
-        content["location"] = content.get("_location", "")
-        content["event_start_time"] = content.get("_event_start_time", "")
-        content["event_end_time"] = content.get("_event_end_time", "")
-        content["_tags"] = content.get("_tags", [])
+
+        event_title = data.get("_event_title", None)
+        if event_title:
+            content["event_title"] = event_title
+
+        description = data.get("_description", None)
+        if description:
+            content["description"] = description
+
+        location = data.get("_location", None)
+        if location:
+            content["location"] = location
+
+        start_time = data.get("_event_start_time", None)
+        if start_time:
+            content["event_start_time"] = start_time
+
+        end_time = data.get("_event_end_time", None)
+        if end_time:
+            content["event_end_time"] = end_time
+
+        tags = data.get("_tags", None)
+        if tags:
+            content["_tags"] = tags
+
+        header_image_url = data.get("_header_image", None)
+        if header_image_url:
+            content["header_image_URL"] = header_image_url
+
+        friendly_creator_name = data.get("_friendly_creator_name", None)
+        if friendly_creator_name:
+            content["friendly_creator_name"] = friendly_creator_name
+
+        reported = data.get("_reported", None)
+        if reported != None:
+            content["reported"] = reported
+
         self.index.partial_update_object(content)
 
     def delete_from_index(self, event_id):
--- flask-server/src/flask_server/services/search_service.py
+++ flask-server/src/flask_server/services/search_service.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_server.global_config import search_client
+from datetime import datetime
 
 search_service = Blueprint('search_service', __name__, url_prefix='/search-service')
 
@@ -8,11 +9,27 @@ def search():
     data = request.json
     query = data.get('query', "")
     filters = data.get('filters', [])
+    start_time = data.get('start_time', None)
+    end_time = data.get('end_time', None)
+    reported = data.get('reported', None)
+    
+    if(reported):
+        return jsonify(search_client.search_index("", "reported:true", None, None))
+
+    if start_time != "" and start_time != None:
+        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%f").timestamp()
+    else:
+        start_time = None
+
+    if end_time != "" and end_time != None:
+        end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S.%f").timestamp()
+    else:
+        end_time = None
 
     # convert search results into query results
     filters = " AND ".join(f"{tag}" for tag in filters)
 
-    search_results = search_client.search_index(query, filters)
+    search_results = search_client.search_index(query, filters, start_time, end_time)
 
     return jsonify(search_results)
 
