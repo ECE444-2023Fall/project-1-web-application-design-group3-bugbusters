61568bd709a5c5839d66f8c1abadc64b6b29882e
Ata Tuzuner <atatuzuner61@gmail.com>
2023-11-10 16:47:55 -0500

    Search endpoint is used for get all events now. Admins can now search reported events.

--- frontend/src/screens/LandingPageScreen.js
+++ frontend/src/screens/LandingPageScreen.js
@@ -4,36 +4,35 @@ import {
   StyleSheet,
   View,
   TextInput,
-  Button,
   TouchableOpacity,
   ScrollView,
   RefreshControl,
-  Pressable,
 } from "react-native";
 import HeaderBar from "../components/HeaderBar";
 import api from "../helpers/API";
 import { useSelector, useDispatch } from "react-redux";
-import { setUserData } from "../store/Action";
 import EventCard from "../components/EventCard";
-import { AntDesign, Ionicons } from "@expo/vector-icons";
+import Checkbox from "expo-checkbox";
+import { Ionicons } from "@expo/vector-icons";
 
 const LandingPageScreen = function ({ navigation }) {
-  // const [text, setText] = useState("");
   const dispatchRedux = useDispatch();
   const primaryColor = useSelector((state) => state.main.primaryColor);
   const secondaryColor = useSelector((state) => state.main.secondaryColor);
   const contrastColor = useSelector((state) => state.main.contrastColor);
+  const userProfileRedux = useSelector((state) => state.userProfileData);
 
   const [events, setEvents] = useState([]);
   const [displaySearchBar, setDisplaySearchBar] = useState(false);
   const [searchText, setSearchText] = useState("");
+  const [reportedEvents, setReportedEvents] = useState(false);
   const refInput = useRef();
 
   // Land or refresh case
   const fetchEvents = async () => {
-    const response = await api.getAllEvents();
+    const response = await api.search({ query: "", filter: [] });
     if (response.result == "SUCCESSFUL") {
-      setEvents(response.data);
+      setEvents(response.data.results);
     } else {
       // TODO: Display error modal
       // console.error("Events cannot be obtained!!\n");
@@ -42,7 +41,11 @@ const LandingPageScreen = function ({ navigation }) {
 
   // Search case (this will be combined with the above function once getAllEvents endpoint is obsolete)
   const searchEvents = async () => {
-    const response = await api.search({ query: searchText, filter: [] });
+    const response = await api.search({
+      query: searchText,
+      filter: [],
+      reported: reportedEvents,
+    });
     if (response.result == "SUCCESSFUL") {
       setEvents(response.data.results);
     } else {
@@ -58,7 +61,7 @@ const LandingPageScreen = function ({ navigation }) {
   return (
     <View style={{ flex: 1, backgroundColor: contrastColor }}>
       <HeaderBar
-        title="Landing Page"
+        title="UofT Events Hub"
         childrenLeft={<View style={{ width: 26 }} />}
         childrenRight={
           <TouchableOpacity
@@ -80,15 +83,15 @@ const LandingPageScreen = function ({ navigation }) {
             <TouchableOpacity
               onPress={() =>
                 navigation.navigate("Event Details", {
-                  event_id: event?._event_id,
+                  event_id: event?.event_id,
                 })
               }
-              key={event?._event_id}
+              key={event?.event_id}
             >
               <EventCard
-                title={event?._event_title}
-                owner={event?._creator_id}
-                image={event?._images?._header_image}
+                title={event?.event_title}
+                owner={event?.friendly_creator_name}
+                image={event?.header_image_URL}
               />
             </TouchableOpacity>
           );
@@ -112,15 +115,29 @@ const LandingPageScreen = function ({ navigation }) {
           returnKeyType="search"
           clearButtonMode="while-editing"
           onChangeText={(text) => {
-            console.log(text);
             setSearchText(text);
           }}
           onSubmitEditing={() => {
-            // TODO: Fire search api
+            searchEvents();
             setDisplaySearchBar(false);
           }}
           onBlur={() => setDisplaySearchBar(false)}
         />
+        <TouchableOpacity
+          style={{
+            ...styles.checkBoxContainer,
+            display: userProfileRedux?.is_admin ? null : "none",
+          }}
+          onPress={() => setReportedEvents(!reportedEvents)}
+        >
+          <Text>Reported Events</Text>
+          <Checkbox
+            style={{ ...styles.checkBox, backgroundColor: contrastColor }}
+            value={reportedEvents}
+            onValueChange={setReportedEvents}
+            color={reportedEvents ? primaryColor : undefined}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -144,6 +161,16 @@ const styles = StyleSheet.create({
     paddingLeft: 6,
     paddingVertical: 4,
     backgroundColor: "white",
+  },
+  checkBoxContainer: {
+    marginBottom: 14,
+    // alignSelf: "center",
+    alignSelf: "flex-end",
+    marginHorizontal: 30,
+    flexDirection: "row",
+  },
+  checkBox: {
+    marginHorizontal: 10,
   },
 });
 
