a9bbd953dde8a22c24e1bc2b36a423ca73faff45
goelbenj <ben_goel@rogers.com>
2023-11-12 03:21:01 -0500

    Add margin to bottom of eventcard scrollview in landing page screen

--- frontend/src/screens/LandingPageScreen.js
+++ frontend/src/screens/LandingPageScreen.js
@@ -106,7 +106,7 @@ const LandingPageScreen = function ({ navigation }) {
         );
       })}
       <ScrollView
-        style={{ marginTop: 4 }}
+        style={{ marginTop: 4, marginBottom: 20 }}
         refreshControl={
           <RefreshControl
             onRefresh={() => {
